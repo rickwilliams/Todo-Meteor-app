9487185f113f1ee82ada7f05bbbb378d4d1e072f
rickwilliams <rick@pixelpod.co.uk>
2014-06-10 01:55:30 +0100

    Create TodoDetails route.

---
+++ client/views/todos/todos_detail/todos_detail.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* TodosDetail: Event Handlers and Helpers */
+/*****************************************************************************/
+Template.TodosDetail.events({
+  /*
+   * Example: 
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.TodosDetail.helpers({
+  /*
+   * Example: 
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* TodosDetail: Lifecycle Hooks */
+/*****************************************************************************/
+Template.TodosDetail.created = function () {
+};
+
+Template.TodosDetail.rendered = function () {
+};
+
+Template.TodosDetail.destroyed = function () {
+};
